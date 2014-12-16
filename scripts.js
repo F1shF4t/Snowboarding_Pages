51d512dd5cd8c102dcf73ed2a5b81bbdb5b66d30
Sergei <f1shf4t@gmail.com>
2014-12-16 22:22:55 +1000

    Test 2

---
+++ scripts.js
@@ -0,0 +1,19 @@
+<script>
+
+//On Page Ready /*
+$(document).ready(function() 
+{
+	window.setInterval(function(){
+		bla();
+	}, 60000);
+	
+	bla();
+}
+
+//Select TSV
+function bla()
+{
+	alert("It Works");
+}
+
+</script>
